b5ca82d1b38edebb071fff4b2cdc2ca6dd3c2b70
Mohammad Yousuf Ali <101050340+uzzal10j@users.noreply.github.com>
2022-11-30 20:59:41 +0600

    test me

--- app/src/main/java/com/example/jurbijur/MainActivity.java
+++ app/src/main/java/com/example/jurbijur/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.jurbijur;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -103,9 +105,33 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+        }
 
-
+    ///=============================
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setMessage("Do you want to Exit?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user pressed "yes", then he is allowed to exit from application
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //if user select "No", just cancel this dialog and continue with app
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
 
 
     }
+    ///===================================
 }
+
